9820e75d7e108ba99fc1904b76623e685fb960e9
myank625 <123228754+myank625@users.noreply.github.com>
2024-07-03 10:31:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+        // Function to open the popup
+        function openPopup() {
+            document.getElementById("popup").style.display = "block";
+        }
+
+        // Function to close the popup
+        function closePopup() {
+            document.getElementById("popup").style.display = "none";
+        }
+
+        // Function to redirect to another page
+        function redirectToPage() {
+            // Replace with the desired URL
+            window.location.href = "https://youtube.com";
+        }
